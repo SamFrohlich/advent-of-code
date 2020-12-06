58e1f445e20d4da6086c683f5648c7f5eb6d9dfb
Samantha Frohlich <sf16540@my.bristol.ac.uk>
2020-12-06 11:05:30 +0000

    6: part 1

---
+++ 6/src/main/scala/Main.scala
@@ -0,0 +1,25 @@
+import scala.io.Source
+import parsley._
+import parsley.Parsley._
+import parsley.Combinator.{some, sepEndBy1}
+import parsley.Char.{letter}
+import parsley.Implicits.{charLift, stringLift}
+import scala.language.implicitConversions
+
+object Main:
+  def main(args: Array[String]): Unit =
+
+    val bufferedSource = Source.fromFile("customs.txt")
+    val customs = bufferedSource.mkString
+    bufferedSource.close
+    val gs = runParser(groups, customs).toOption.get
+    println(count(gs))
+
+def count(gs : List[List[List[Char]]]): Int =
+  gs.map(_.flatten.distinct.length).sum
+
+// parser - for separating people and groups
+
+val person : Parsley[List[Char]] = some(letter)
+val group : Parsley[List[List[Char]]] = sepEndBy1(person, '\n')
+val groups : Parsley[List[List[List[Char]]]] = sepEndBy1(group, '\n')
---
+++ 6/src/test/scala/Test1.scala
@@ -0,0 +1,9 @@
+import org.scalatest.Assertions
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import scala.language.implicitConversions
+import scala.language.postfixOps
+
+// class Test1 extends AnyFlatSpec with Matchers with Assertions:
+//     "Main.msg" should "be a dotty welcome message" in:
+//         Main.msg should equal("I was compiled by dotty :)")
